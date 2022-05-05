8394de5777bee52341f2428e4c1f311cecb24a12
Lucas Bevilaqua Araujo de Oliveira <laobevilaqua@outlook.com>
2022-05-05 14:48:30 -0300

    fix: change createConnection for createPool

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ export async function connect() {
       return global.connection;
   const mysql = require("mysql2/promise");
 
-  const connection = await mysql.createConnection(databaseUrl);
+  const connection = await mysql.createPool(databaseUrl);
   console.log("Conectou no MySQL!");
 
   global.connection = connection;
